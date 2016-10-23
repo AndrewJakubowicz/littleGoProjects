a7a5e10c829f9003fba51ba1e84d41da4804e85e
SpyR1014 <spyr1014@gmail.com>
2016-10-23 18:24:14 +1100

    Added Trivial concurrency exercise 1

---
+++ trivialConcurrencyExercises/theDailyWalk/main.go
@@ -0,0 +1,9 @@
+// Exercise 1: The Daily Walk.
+// Every morning, Alice and Bob go for a walk.
+// They both grab sunglasses, perhaps a belt, closing open windows, turning off ceiling fans,
+// and pocketing their phones and keys.
+// Once they're BOTH ready, which typically takes between 60 - 90 seconds,
+// they arm the alarm, which has a 60 second delay.
+// While the alarm counts down they both put on their shoes (35 - 45 seconds).
+// They then leave the house together and lock the door before the alarm has finished the countdown.
+package main
